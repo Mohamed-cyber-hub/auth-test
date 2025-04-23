6817027af949157c490ef7fe7e4ffaae703efd6e
Mohamed-cyber-hub <Mohamed.saad.tradelinestores@gmail.com>
2025-04-23 21:59:43 +0200

    update

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-    origin: "https://auth-test-frontend.vercel.app",
+    origin: "http://localhost:5173",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
 }))
