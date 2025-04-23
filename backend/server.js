1ccd201e1b69e46d2368f3d487d143119b367ccf
Mohamed-cyber-hub <Mohamed.saad.tradelinestores@gmail.com>
2025-04-23 23:00:21 +0200

    update

--- backend/server.js
+++ backend/server.js
@@ -10,11 +10,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({
-    origin: ["https://auth-test-frontend.vercel.app"],
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-}))
+app.use(cors())
 
 app.use(express.json());
 app.use(cookieParser());
